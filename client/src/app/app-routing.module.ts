1ad0b7f2759c725c8f85c1e28904480b72a81d2b
Benoit BERTRAND <benoit.bertrand@live.fr>
2021-01-18 00:25:37 +0100

    [client] Game template

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SiteComponent } from './pages/site/site.component';
+import { GameComponent } from './pages/game/game.component';
 
 const routes: Routes = [
-  {path: 'home',      component:SiteComponent}
+  {path: 'home',      component:SiteComponent},
+  {path: 'game',      component:GameComponent}
 ];
 
 @NgModule({
