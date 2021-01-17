f2354b53ad63205ea6b15aea91c1fad8370c5804
Benoit BERTRAND <benoit.bertrand@live.fr>
2021-01-17 05:14:16 +0100

    [client] Intro Star Wars

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SiteComponent } from './pages/site/site.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home',      component:SiteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
